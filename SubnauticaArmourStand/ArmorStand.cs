cbca6dbf3f1feee48f8c90214daba608050aa60d
Ovyerus <iamovyerus@gmail.com>
2018-12-31 15:29:23 +1100

    feat: basic implementation - no actual functionality yet

---
+++ SubnauticaArmourStand/ArmorStand.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using SMLHelper.V2.Assets;
+using SMLHelper.V2.Crafting;
+using UnityEngine;
+
+namespace SubnauticaArmourStand
+{
+    class ArmourStand : Buildable
+    {
+        public override string AssetsFolder => Main.AssetFolder;
+        public override TechGroup GroupForPDA => TechGroup.InteriorModules;
+        public override TechCategory CategoryForPDA => TechCategory.InteriorModule;
+        public override string HandOverText => "Access armour stand";
+        public override string IconFileName => "ArmourStand.png";
+
+        public ArmourStand() : base("ArmourStand", "Armour Stand", "Show off and store your various suits.") { }
+
+        protected override TechData GetBlueprintRecipe()
+        {
+            List<Ingredient> ingredients = new List<Ingredient>()
+            {
+                new Ingredient(TechType.Titanium, 2),
+                new Ingredient(TechType.Glass, 1)
+            };
+            TechData recipe = new TechData()
+            {
+                craftAmount = 1,
+                Ingredients = ingredients
+            };
+
+
+            return recipe;
+        }
+
+        public override GameObject GetGameObject() {
+            GameObject armourStand = Object.Instantiate(Main.ArmourStandModel);
+            Constructable armourStandConstructable = armourStand.AddComponent<Constructable>();
+
+            armourStandConstructable.allowedOnWall = false;
+            armourStandConstructable.allowedOnGround = true;
+            armourStandConstructable.allowedInSub = true;
+            armourStandConstructable.allowedOutside = true;
+            armourStandConstructable.techType = this.TechType;
+            armourStandConstructable.model = armourStand.transform.GetChild(0).gameObject;
+
+            return armourStand;
+        }
+    }
+}
---
+++ SubnauticaArmourStand/SubnauticaArmourStand.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection;
+using Harmony;
+using UnityEngine;
+
+namespace SubnauticaArmourStand
+{
+    public class Main
+    {
+        public const string AssetFolder = "SubnauticaArmourStand/Assets";
+        public const string AssetsBundle = "./QMods/" + AssetFolder + "/resources";
+        public static GameObject ArmourStandModel { private set; get; }
+
+        public static void Init()
+        {
+            HarmonyInstance harmony = HarmonyInstance.Create("ovyerus.ArmourStand.mod");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+
+            new ArmourStand().Patch();
+
+            Load();
+        }
+
+        private static void Load()
+        {
+            /*AssetBundle bundle = AssetBundle.LoadFromFile(AssetsBundle);
+
+            ArmourStandModel = bundle.LoadAsset("ArmourStandModel") as GameObject;*/
+        }
+    }
+}
